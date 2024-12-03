e380cb061f7b83145effd3669d7620c72b78d3bd
vikram17dp <vikramdp505@gmail.com>
2024-12-04 01:14:30 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import './App.css'
 
@@ -9,15 +8,6 @@ function App() {
   <div>Hello</div>
        
        
-=======
-import React from 'react'
-
-function App() {
-  return (
-    <div>
-      <h1>Home page</h1>
-    </div>
->>>>>>> 13ed79d4551667956a3b2a27602e24ef89d28561
   )
 }
 
