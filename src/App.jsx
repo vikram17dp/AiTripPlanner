ab3ef0e4e255a882563f2b54aceeca81f0e8adbf
vikram17dp <vikramdp505@gmail.com>
2024-12-04 15:41:27 +0530

    setting up the routes to the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-
-import './App.css'
-import { Button } from './components/ui/button'
-
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./create-trip/Home";
+import CreateTrip from "./create-trip/CreateTrip";
 function App() {
- 
-
   return (
-  <div>
-    <h2>welcome to project</h2>
-    <Button>Hello</Button>
-
-  </div>
-       
-       
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-trip" element={<CreateTrip />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
