d6315e56af2a41d3e02dff480ca5fe2b80b8acb5
vikram17dp <vikramdp505@gmail.com>
2024-12-04 15:53:49 +0530

    adding header and stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./create-trip/Home";
-import CreateTrip from "./create-trip/CreateTrip";
+import Hero from "./components/custom/Hero";
+
+
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-trip" element={<CreateTrip />} />
-      </Routes>
+      <Hero/>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,25 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import {  createBrowserRouter, RouterProvider } from "react-router-dom";
+import CreateTrip from "./create-trip/CreateTrip";
+import Header from "./components/custom/Header";
 
+const router = createBrowserRouter([
+  {
+    path:"/" ,
+    element:<App/>
+  },
+  {
+    path:'/create-trip',
+    element:<CreateTrip/>
+  }
+])
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
     <StrictMode>
-      <App />
+      <Header/>
+      <RouterProvider router={router}/>
+      
     </StrictMode>
     
-  </BrowserRouter>
 );
