57c3936cf2e728f75983f06474b94b1c14e308b8
vikram17dp <vikramdp505@gmail.com>
2024-12-10 15:37:53 +0530

    last changes

--- api/index.js
+++ api/index.js
@@ -15,11 +15,14 @@ connectCloudinary();
 const port = process.env.PORT || 4000;
 
 app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-  credentials: true,
+  origin: 'https://ai-trip-planner-fullstack.vercel.app', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,  
 }));
+
+app.options('*', cors());
+
 
 app.use("/api/user", userRouter);
 
